cec1e20632df2783e2d2d44c3a067ce9215b5fcc
unknown <matej.pavic92@gmail.com>
2020-10-26 12:55:10 +0100

    Updates

---
+++ precache-manifest.f14ad72253bafeb6c5c5dab8e9587729.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8961e7acf2f8ba0a81689a6a42fc4754",
+    "url": "/LOTG/index.html"
+  },
+  {
+    "revision": "671c6f671713c13c35b3",
+    "url": "/LOTG/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eefbcb646b293cc26dd5",
+    "url": "/LOTG/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "671c6f671713c13c35b3",
+    "url": "/LOTG/static/js/2.c82c63f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LOTG/static/js/2.c82c63f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eefbcb646b293cc26dd5",
+    "url": "/LOTG/static/js/main.190e8271.chunk.js"
+  },
+  {
+    "revision": "57f27efc45f85a7da191",
+    "url": "/LOTG/static/js/runtime-main.eb6ea52f.js"
+  },
+  {
+    "revision": "d8e02694419d32e631b761845e2ff217",
+    "url": "/LOTG/static/media/NSZSDGrb.d8e02694.svg"
+  }
+]);
